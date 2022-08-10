675955984e8fbe4331f5c2fc753472e5959aec59
PrioD <loszlove55@gmail.com>
2022-08-10 17:32:36 -0300

    Bháskara braboza

--- Controllers/TestControler.cs
+++ Controllers/TestControler.cs
@@ -9,4 +9,39 @@ public class TestController:ControllerBase
     {
         return "My app is Working!";
     }
+    [HttpGet("quadratica/{x}")]
+    public string Get(int x,int a,int b, int c)
+    {
+        int resultado = a*x*x+b*x+c;
+        return resultado.ToString();
+    }
+    [HttpGet ("solucao")]
+    public object Get(double a = 1 , double b=0,double c = 0)
+    {
+        if(a==0)
+        {
+            return new {
+                status = "Fail",
+                message = "'a' nao pode ser zero."
+            };
+
+        }
+        double delta = (b*b-4*a*c);
+        
+        if(delta <0){
+           return new 
+           {
+                status = "Fail",
+                message = " delta nao podse ser zero."
+           };
+        }
+            
+        double x1 = (-b + Math.Sqrt(delta))/a;
+        double x2 = (-b - Math.Sqrt(delta))/a;
+        return new {
+            status = "Sucesso",
+            solucao1 = x1,
+            solucao2 = x2,
+        };
+    }
 }
